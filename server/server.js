246d8296a3cede3ed29bb7ea595fd6c5f7cbcd3f
Venkataramana hegde <venkataramanahegde64@gmail.com>
2025-01-08 22:02:29 +0530

    add cors package

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://mern-auth-1-2coj.onrender.com'
+ ]
 
 app.use(express.json());
 app.use(cookieParser());
